4661525ed0a447b9c16a4aa84bcda573a372d66b
sylvia <stan@smith.edu>
2024-11-09 06:51:02 -0500

    nvalidate

--- Back-End/index.js
+++ Back-End/index.js
@@ -5,46 +5,51 @@ const uri = "mongodb+srv://sylvia:4xs2pp1R6EVSd8k5@parkinglotfinder.jj5d2.mongod
 const client = new MongoClient(uri);
 
 const database = client.db('parkingdb');
-const samples = database.collection('parking_lot');
+const parking_lot = database.collection('parking_lot');
+const unvalidated = database.collection('invalid');
 
-async function run() {
+async function find([lat,lon]) {
 
     try {
   
-      const query = {weekday : 1};
+      const query = {location:[lat,lon]};
   
-      const sample = await samples.findOne(query);
+      const parking_lot = await parking_lot.findOne(query);
   
-      console.log(sample)
+      console.log(parking_lot)
   
     } finally {
   
       // Ensures that the client will close when you finish/error
       await client.close();
-
     }
-  
   }
 
-  async function write() {
-
+  async function write({
+    location: [lat,lon],
+    price: price_input,
+    occupation: occupation_input
+  }) {
     try {
+        // 10 random records generation
+        // const generateRandomDocument = () => ({
+        //     _id: Math.floor(Math.random() * 100) + 1,
+        //     location: [Math.floor(Math.random() * 100) + 1,Math.floor(Math.random() * 100) + 1], // Generates a new ObjectId
+        //     price: Math.floor(Math.random() * 100) + 1, // Random price between 1 and 100
+        //     occupation: Math.floor(Math.random() * 100), // Random occupation percentage between 0 and 100
+        // });
 
-        const generateRandomDocument = () => ({
-            _id: Math.floor(Math.random() * 1000) + 1, // Generates a new ObjectId
-            weekday: Math.floor(Math.random() * 7) + 1, // Random integer between 1 and 7
-            ticket: new Date(2000, 0, 1, 5), // Fixed Date: 2000-01-01T05:00:00.000+00:00
-            time: new Date(2000, 0, 1, 5), // Fixed Date: 2000-01-01T05:00:00.000+00:00
-            price: Math.floor(Math.random() * 100) + 1, // Random price between 1 and 100
-            occupation: Math.floor(Math.random() * 100), // Random occupation percentage between 0 and 100
+        // // Generate an array of 10 random documents
+        // const randomDocuments = Array.from({ length: 10 }, generateRandomDocument);
+
+        // // Insert the documents into MongoDB
+        // const result = await unvalidated.insertMany(randomDocuments);
+        const result = await unvalidated.insertOne({
+          location: [lat,lon],
+          price: price_input,
+          occupation: occupation_input
         });
 
-        // Generate an array of 10 random documents
-        const randomDocuments = Array.from({ length: 10 }, generateRandomDocument);
-
-        // Insert the documents into MongoDB
-        const result = await samples.insertMany(randomDocuments);
-        
         console.log(result); 
 
     } finally {
@@ -55,4 +60,8 @@ async function run() {
 
 }
 
-write().catch(console.dir);
+write({
+  location: [1,1],
+  price: 2,
+  occupation: 3
+}).catch(console.dir);
