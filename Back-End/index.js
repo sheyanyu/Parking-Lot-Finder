aa29009343d74eef02e43274151a8972ae26f7e9
sheyanyu <sheyanyu@umass.edu>
2024-11-09 09:11:33 -0500

    change

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,29 +1,58 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
 const { MongoClient } = require("mongodb");
-
 const uri = "mongodb+srv://sylvia:4xs2pp1R6EVSd8k5@parkinglotfinder.jj5d2.mongodb.net/?retryWrites=true&w=majority&appName=ParkingLotFinder";
-
 const client = new MongoClient(uri);
-
 const database = client.db('parkingdb');
 const parking_lot = database.collection('parking_lot');
 const unvalidated = database.collection('invalid');
 
-async function find([lat,lon]) {
+async function find() {
 
     try {
   
-      const query = {location:[lat,lon]};
+      const query = {weekday:1};
   
-      const parking_lot = await parking_lot.findOne(query);
-  
-      console.log(parking_lot)
-  
+      const dataset = await parking_lot.findOne(query);
+      console.log(dataset)
+      
+      // Middleware to parse JSON request bodies
+      app.use(express.json());
+
+      // API endpoint to POST data (add to the dataset)
+      app.post('/add-item', (req, res) => {
+        const newItem = req.body;
+
+        // Add new item to dataset
+        dataset.push(newItem);
+
+        // Respond with the updated dataset
+        res.status(201).json({
+          message: 'Item added successfully',
+          data: dataset,
+        });
+      });
+
+      // API endpoint to GET all items in the dataset
+      app.get('/items', (req, res) => {
+        res.json(dataset);
+      });
+
+      // Start the server
+      app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+      });
+        
     } finally {
   
       // Ensures that the client will close when you finish/error
       await client.close();
     }
   }
+  find();
+
 
   async function write({
     location: [lat,lon],
@@ -44,6 +73,7 @@ async function find([lat,lon]) {
 
         // // Insert the documents into MongoDB
         // const result = await unvalidated.insertMany(randomDocuments);
+        
         const result = await unvalidated.insertOne({
           location: [lat,lon],
           price: price_input,
@@ -60,8 +90,8 @@ async function find([lat,lon]) {
 
 }
 
-write({
-  location: [1,1],
-  price: 2,
-  occupation: 3
-}).catch(console.dir);
+// write({
+//   location: [1,1],
+//   price: 2,
+//   occupation: 3
+// }).catch(console.dir);
